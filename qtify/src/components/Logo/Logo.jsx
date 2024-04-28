defbdeb1425a6f74df144ac2106494e88d9f9df0
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-04-28 16:57:46 +0530

    Redesigned NavBar Entirely With Plain Css

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import logoImg from '../../assets/logo.png'
-import { Link } from "@mui/material";   
+
 export default function Logo(){
-    return <img src={logoImg} style={{width:"67px",height:"34px"}}/>
+    const styleImg = {
+        width:"67px",
+        height:"34px"
+    }
+
+    return <img src={logoImg} style={styleImg}/>
 }
--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,46 +1,17 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import Logo from '../Logo/Logo';
 import SearchBar from '../SearchBar/Searchbar';
+import Button from '../Button/Button'
 import { useTheme } from '@mui/material/styles';
+import styles from './navbar.module.css'
 
 export default function Navbar() {
-  const theme = useTheme();
-
+  
   return (
-    <Box sx={{ flexGrow: 1}}>
-      <AppBar position="static" sx={{ background: theme.palette.green }}>
-        <Toolbar sx={{justifyContent: "space-between"}}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            borderRadius="0px"
-          >
-            <Logo />
-            
-          </IconButton>
-          <SearchBar />
-          <Button  
-          sx={{ 
-            background:theme.palette.black ,
-            color:theme.palette.green,
-            fontWeight: 600,
-            textTransform: "none",
-            padding: "10px 16px",
-            borderRadius: "12px",
-            fontSize: "18px"
-          }} >
-            Give Feedback
-            </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <nav className={styles.navbar}>
+      <Logo />
+      <SearchBar />
+      <Button />
+    </nav>
   );
 }
