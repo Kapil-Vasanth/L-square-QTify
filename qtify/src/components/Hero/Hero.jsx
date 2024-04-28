73e2b738f85c49db9449b9b67ddb2632d5645421
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-04-28 18:40:01 +0530

    fixed Hero Section

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import HeroHeadPhones from '../../assets/hero_headphones.png'
 export default function Hero(){
     return <section className={styles.heroSection}>
         <h4 className={styles.heroTitle}>100 Thousand Songs, ad-free <br /> Over thousands podcast episodes</h4>
-        <img src={HeroHeadPhones} alt="headphones-img" />
+        <img className={styles.heroHeadPhone} src={HeroHeadPhones} alt="headphones-img" />
     </section>
 }
 
