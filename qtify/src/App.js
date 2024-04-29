25c0f6d3ea401bdd6946b489fb9bbb57817a1a98
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-04-30 01:44:54 +0530

    Fixed Fetching New Songs and Top Albums

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,17 +1,44 @@
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero'
 import { ThemeProvider } from '@mui/material/styles';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import theme from './theme'
 import './App.css';
 import Carousel from './components/Carousel/Carousel';
 
 function App() {
+  const [topSongs,setTopSongs] = useState([])
+  const [newSongs,setNewSongs] = useState([])
+  useEffect(() => {
+    async function fetchTopAlbums(){
+        try{
+            let res = await axios.get('https://qtify-backend-labs.crio.do/albums/top');
+            setTopSongs(res.data)
+        }catch(err){
+            alert(err);
+        }
+    }
+    async function fetchNewAlbums(){
+      try{
+          let res = await axios.get('https://qtify-backend-labs.crio.do/albums/new');
+          setNewSongs(res.data)
+      }catch(err){
+          alert(err);
+      }
+  }
+    fetchTopAlbums();
+    fetchNewAlbums();
+},[])
+
+
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <Navbar />
         <Hero />
-        <Carousel />
+        <Carousel data={topSongs} title="Top Albums"/>
+        <Carousel data={newSongs} title="New Albums"/>
       </div>
     </ThemeProvider>
   );
--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import logoImg from '../../assets/logo.png'
 import styles from './logo.module.css'
 
 export default function Logo(){
-    return <div>
+    return <div className={styles.logoContainer}>
         <img src={logoImg} className={styles.logoImg} alt='LogoImg'/>
     </div>
 }
